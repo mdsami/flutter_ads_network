66d7c35c9e90452b92d94b14ec09e943b8988e3a
sanjidbillah <raiyansafir@gmail.com>
2020-04-19 02:27:31 +0600

    nothing

---
+++ lib/Screen/HomePage.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hey Ads!'),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+          itemCount: AdsList.length,
+          itemBuilder: (BuildContext, index) {
+            return RaisedButton(
+              color: Colors.white,
+              onPressed: () {},
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(AdsList[index].Adsname),
+                  Image.network(
+                    "https://pluspng.com/img-png/google-logo-png-open-2000.png",
+                    height: size.height * 0.03,
+                    width: size.width * 0.05,
+                  )
+                ],
+              ),
+            );
+          }),
+    );
+  }
+
+  List<AdsData> AdsList = [
+    AdsData("Banner", "Google", "Banner"),
+    AdsData("InterStital", "Google", ""),
+    AdsData("Rewarded", "Google", ""),
+    AdsData("Native Advanced", "Google", ""),
+  ];
+}
+
+class AdsData {
+  final String Adsname;
+  final String Provider;
+  final String GoTo;
+
+  AdsData(this.Adsname, this.Provider, this.GoTo);
+}
---
+++ lib/google/banner.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Banner extends StatefulWidget {
+  @override
+  _BannerState createState() => _BannerState();
+}
+
+class _BannerState extends State<Banner> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
